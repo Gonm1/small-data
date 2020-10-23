a42b6bf2a591c4ed780a9b0710defbc504b9a0ca
Gonm1 <gonzalomiranda49@hotmail.com>
2020-10-23 20:48:57 -0300

    Added graphs

--- mnist/dnn.py
+++ mnist/dnn.py
@@ -6,17 +6,7 @@ from keras.losses import CategoricalCrossentropy
 from keras.callbacks import EarlyStopping
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras import backend as K
-
-def F1Measure(y_true, y_pred): #taken from old keras source code
-    "Function from https://medium.com/@aakashgoel12/how-to-add-user-defined-function-get-f1-score-in-keras-metrics-3013f979ce0d"
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    recall = true_positives / (possible_positives + K.epsilon())
-    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
-    return f1_val
+from utils import F1Measure
 
 def dnn(x_train, y_train, x_test, y_test, ep, bs, verb=0):
     num_classes = y_test.shape[1]
@@ -38,16 +28,16 @@ def dnn(x_train, y_train, x_test, y_test, ep, bs, verb=0):
     model.compile(loss=CategoricalCrossentropy(), optimizer=Adam(lr=0.001), metrics=[CategoricalAccuracy(), Precision(), Recall(), F1Measure])
 
     if verb: model.summary()
-    
-    earlyStop = EarlyStopping(monitor='loss', mode='min', patience=2)
-    model.fit(x_train, y_train, epochs=ep, batch_size=bs, verbose=verb, callbacks=[earlyStop])
+
+    earlyStop = EarlyStopping(monitor='val_loss', mode='min', patience=2, verbose=verb)
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=ep, batch_size=bs, verbose=verb, callbacks=[earlyStop])
 
     loss, accuracy, precision, recall, f1 = model.evaluate(x_test, y_test, batch_size=bs, verbose=verb)
-    #return f"val_loss: {round(loss,4)}\nval_accuracy: {round(accuracy,4)}\nval_precision: {round(precision,4)}\nval_recall: {round(recall,4)}\nval_f1: {round(f1,4)}\n"
-    return round(loss,4), round(accuracy,4), round(precision,4), round(recall,4), round(f1,4)
-    
+    return history, round(loss,4), round(accuracy,4), round(precision,4), round(recall,4), round(f1,4)
+
 if __name__ == "__main__":
     from mnistloader import load_mnist, mnist_preprocess
+    from utils import make_graphs
     import tensorflow as tf
     import numpy as np
     import random
@@ -73,14 +63,16 @@ if __name__ == "__main__":
         # Pre process images
         x_train, y_train, x_test, y_test = mnist_preprocess(x_train, y_train, x_test, y_test)
         print("Shape after pre processing: ", x_train.shape, y_train.shape, x_test.shape, y_test.shape)
-
+        
         print(f"Training set size: {len(x_train)}")
         print(f"Test set size: {len(x_test)}", end='\n\n')
 
         epochs = 20
         batch_size = 32
-        loss, accuracy, precision, recall, f1 = dnn(x_train, y_train, x_test, y_test, epochs, batch_size, verb=1)
+        history, loss, accuracy, precision, recall, f1 = dnn(x_train, y_train, x_test, y_test, epochs, batch_size, verb=1)
         results[index] = item*10, loss, accuracy, precision, recall, f1
+
+        make_graphs(history, item, 'dnn')
 
     np.set_printoptions(suppress=True)
     print(results)
