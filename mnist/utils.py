c773f1771093cd8ccd718e4fd40898151144720c
Gonm1 <gonzalomiranda49@hotmail.com>
2020-11-03 11:46:00 -0300

    Changed plotting func

--- mnist/utils.py
+++ mnist/utils.py
@@ -1,39 +1,20 @@
-def make_graphs(history, item, prefix=''):
+def make_graphs(histories, items, prefix=''):
     from matplotlib import pyplot
     keys = list()
-    for key in history.history.keys():
+    for key in histories[0].history.keys():
         keys.append(key)
-
-    fig, axs = pyplot.subplots(1, 5, constrained_layout=True, figsize=(18,4))
+    fig, axs = pyplot.subplots(2, 4, constrained_layout=True, figsize=(18,7))
     fontsize=12
+    for index, history in enumerate(histories):
+        axs[0][index].set_title(f"Items por clase: {items[index]}")
+        axs[0][index].plot(history.history[keys[0]], '--', label='train')
+        axs[0][index].plot(history.history[keys[2]], label='test')
+        axs[0][0].set_ylabel('Loss', fontsize=fontsize)
 
-    fig.suptitle(f"Metricas sobre un conjunto de entrenamiento de tamaño {item*10}", fontsize=fontsize+2)
-
-    axs[0].plot(history.history[keys[0]], label='train')
-    axs[0].plot(history.history[keys[5]], label='test')
-    axs[0].set_xlabel('Iteraciones', fontsize=fontsize)
-    axs[0].set_ylabel('Loss', fontsize=fontsize)
-    
-    axs[1].plot(history.history[keys[1]], label='train')
-    axs[1].plot(history.history[keys[6]], label='test')
-    axs[1].set_xlabel('Iteraciones', fontsize=fontsize)
-    axs[1].set_ylabel('Accuracy', fontsize=fontsize)
-
-    axs[2].plot(history.history[keys[2]], label='train')
-    axs[2].plot(history.history[keys[7]], label='test')
-    axs[2].set_xlabel('Iteraciones', fontsize=fontsize)
-    axs[2].set_ylabel('Precision', fontsize=fontsize)
-    axs[2].legend()
-
-    axs[3].plot(history.history[keys[3]], label='train')
-    axs[3].plot(history.history[keys[8]], label='test')
-    axs[3].set_xlabel('Iteraciones', fontsize=fontsize)
-    axs[3].set_ylabel('Recall', fontsize=fontsize)
-
-    axs[4].plot(history.history[keys[4]], label='train')
-    axs[4].plot(history.history[keys[9]], label='test')
-    axs[4].set_xlabel('Iteraciones', fontsize=fontsize)
-    axs[4].set_ylabel('F1 Score', fontsize=fontsize)
-
-    pyplot.savefig(f"{prefix}-{item*10}-metrics.pdf")
+        axs[1][index].plot(history.history[keys[1]], '--', label='train')
+        axs[1][index].plot(history.history[keys[3]], label='test')
+        axs[1][index].set_xlabel('Iteraciones', fontsize=fontsize)
+        axs[1][0].set_ylabel('Accuracy', fontsize=fontsize)
+    axs[0][0].legend()
+    pyplot.savefig(f"{prefix}-metrics.pdf")
     pyplot.close()
