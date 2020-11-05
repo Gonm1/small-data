b3a08cdd3555fab1289e2c67111a12f2f9062dc1
Gonzalo <gonzalomiranda49@hotmail.com>
2020-11-05 22:26:49 +0000

    messages

--- mnist/runner.sh
+++ mnist/runner.sh
@@ -2,6 +2,8 @@ for file in $(ls | grep -F .py)
 do
     if [ $file != 'mnistloader.py' ] && [ $file != 'utils.py' ]
     then
-        python3 $file
+        echo "$file started."
+        time python3 $file
+        echo "$file done."
     fi
 done
