02bb82cb2be938781fecdc63cc5ece107b413575
Gonzalo <gonzalomiranda49@hotmail.com>
2020-12-09 02:03:29 +0000

    deleted runner.sh as it is not needed

--- mnist/runner.sh
+++
@@ -1,9 +0,0 @@
-for file in $(ls | grep -F .py)
-do
-    if [ $file != 'mnistloader.py' ] && [ $file != 'utils.py' ]
-    then
-        echo "$file started."
-        time python3 $file
-        echo "$file done."
-    fi
-done
